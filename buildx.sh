4421a8e0acd3f91ac53abc452918c0c9f6afa0cc
Philip Lehmann <philiplehmann@gmail.com>
2023-04-10 18:40:18 +0200

    use self-hosted runner

--- buildx.sh
+++ buildx.sh
@@ -1,6 +1,6 @@
 set -x
 
 docker buildx create --platform linux/arm64,linux/amd64,linux/ppc64le,linux/s390x --name unoserver-builder --use
-docker buildx build --push --no-cache --platform=linux/arm64,linux/amd64,linux/ppc64le,linux/s390x --tag=philiplehmann/unoserver:v0.0.1 .
+docker buildx build --push --no-cache --platform=linux/arm64,linux/amd64 --tag=philiplehmann/unoserver:v0.0.1 .
 
-docker buildx imagetools create registry.hub.docker.com/philiplehmann/unoserver:v0.0.1 --tag registry.hub.docker.com/philiplehmann/unoserver:latest
+docker buildx imagetools create philiplehmann/unoserver:v0.0.1 --tag philiplehmann/unoserver:latest
